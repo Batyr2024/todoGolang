13af14dd49d818c97ef9968898f8f7e0be05a256
Batyr Abdusalamov <b.abdusalamov@nv.dunice.net>
2024-09-05 17:09:10 +0300

    Server operation has been restored

--- internal/api/server.go
+++ internal/api/server.go
@@ -2,14 +2,14 @@ package api
 
 import (
 	"github.com/Batyr2024/todoGolang/internal/api/handlers"
+	"github.com/Batyr2024/todoGolang/internal/repository"
+	"github.com/Batyr2024/todoGolang/internal/usecase"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-var taskHandler = handlers.NewTaskHandler()
-
 func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
-	h := &taskHandler{}
+	h := handlers.NewTaskHandler(usecase.NewTaskUseCase(repository.NewTaskRepository(db)))
 
 	routes := r.Group("/tasks")
 		routes.GET("/", h.FindAll)
